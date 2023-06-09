bf23eb83919bdf2cae93b2ab1c6e24d091ccf44f
tingul4 <xpple413208@gmail.com>
2023-06-09 15:06:16 +0800

    Init

---
+++ app.js
@@ -0,0 +1,57 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// routes setting
+app.get('/', (req, res) => {
+  res.send(`<h1>Create your own server with Node.js</h1>
+<h3>Popular movies in 2018</h3>
+<ul>
+  <li>
+    <img
+      src="https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg"
+      alt="Jurassic World: Fallen Kingdom"
+      width="50px"
+    />
+    Jurassic World: Fallen Kingdom
+  </li>
+  <li>
+    <img
+      src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg"
+      alt="Ant-Man and the Wasp"
+      width="50px"
+    />
+    Ant-Man and the Wasp
+  </li>
+  <li>
+    <img
+      src="https://movie-list.alphacamp.io/posters/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg"
+      alt="Thor: Ragnarok"
+      width="50px"
+    />
+    Thor: Ragnarok
+  </li>
+  <li>
+    <img
+      src="https://movie-list.alphacamp.io/posters/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+      alt="Avengers: Infinity War"
+      width="50px"
+    />
+    Avengers: Infinity War
+  </li>
+  <li>
+    <img
+      src="https://movie-list.alphacamp.io/posters/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg"
+      alt="Mission: Impossible - Fallout"
+      width="50px"
+    />
+    Mission: Impossible - Fallout
+  </li>
+</ul>`)
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
