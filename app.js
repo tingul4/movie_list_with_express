5fd4fb8a2fc70edd46d59e77dd7dc30546cff19f
tingul4 <xpple413208@gmail.com>
2023-06-09 16:46:41 +0800

    Use static file (bootstrap/popper) and add CSS

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 // routes setting
 app.get('/', (req, res) => {
   res.render('index')
