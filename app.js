2688d8144666627451d8cab3f31772a3e4cab111
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-12 23:35:58 -0400

    Make show page

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ app.get('/', (req, res) => {
   res.render('index', { movies: movieList.results })
 })
 
+app.get('/movies/1', (req, res) => {
+  const movieOne = {
+    id: 1,
+    title: 'Jurassic World: Fallen Kingdom',
+    description:
+      'Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.',
+    release_date: '2018-06-06',
+    image: 'c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
+  }
+  res.render('show', { movie: movieOne })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
