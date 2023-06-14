4cec583d8bdc6230441e656943af2f1ca8a5397a
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-14 11:13:31 -0400

    Use "query string" to make search function

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.get('/movies/:movie_id', (req, res) => {
   res.render('show', { movie: movie })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const movies = movieList.results.filter(movie => {
+    return movie.title.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { movies: movies, keyword: keyword })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
